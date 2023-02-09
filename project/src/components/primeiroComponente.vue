cbed2acb65ae3dca52a782dd3bcfcdc59e31226c
rxfaelr <rafa331rodri@gmail.com>
2023-02-09 10:23:51 -0300

    component's data

--- project/src/components/primeiroComponente.vue
+++ project/src/components/primeiroComponente.vue
@@ -1,9 +1,18 @@
 <template>
+    <div>
     <h1>olá vue</h1>
+    <P>meu nome é {{ nome }} e trabalho como {{ profissao }}</P>
+</div>
 </template>
 
 <script>
 export default {
-    name:'PrimeiroComponente'
+    name:'PrimeiroComponente',
+    data (){
+        return {
+            nome: "rafael",
+            profissao: "programador"
+        }
+    }
 }
 </script>
