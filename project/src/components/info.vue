38dc2720b8f43ec6f86471394ec3817d766a6dae
rxfaelr <rafa331rodri@gmail.com>
2023-02-10 11:44:19 -0300

    diretivas

--- project/src/components/info.vue
+++ project/src/components/info.vue
@@ -1,18 +1,28 @@
 <template>
 <div>
     <p>
-        Estou trabalhando, utilizando as seguintes tecnologias:
+        <p v-if="esta_trabalhando">Estou trabalhando</p>
+        <p v-else>NAO TRABALHO</p>
+        <p>utilizando as seguintes tecnologias:</p>
     <ul>
         <li>Vue.js</li>
         <li>Flutter</li>
         <li>C#</li>
     </ul>
+    <p v-show="mostrar_email">mande um email para {{ email }}</p>
     </p>
 </div>
 </template>
 
 <script>
 export default {
-    name: 'Info'
+    name: 'Info',
+    data () {
+        return {
+            esta_trabalhando: false,
+            mostrar_email: true,
+            email: 'rafael@gmail.com'
+        }
+    }
 }
 </script>
