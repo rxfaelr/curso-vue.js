ea2a0d49ceef2db9cf205bbe807c80ab1d18899a
rxfaelr <rafa331rodri@gmail.com>
2023-02-11 22:49:48 -0300

    atributos dinamicos

--- project/src/components/info.vue
+++ project/src/components/info.vue
@@ -10,18 +10,25 @@
         <li>C#</li>
     </ul>
     <p v-show="mostrar_email">mande um email para {{ email }}</p>
+    <p>Para acessar meu portifolio basta <a v-bind:href="meu_link">basta clicar aqui</a></p>
+    <foto/>
     </p>
 </div>
 </template>
 
 <script>
+import foto from './picture.vue';
 export default {
     name: 'Info',
+    components: {
+        foto
+    },
     data () {
         return {
             esta_trabalhando: false,
             mostrar_email: true,
-            email: 'rafael@gmail.com'
+            email: 'rafael@gmail.com',
+            meu_link: 'https://rxfaelr.github.io/my-portfolio/'
         }
     }
 }
---
+++ project/src/components/picture.vue
@@ -0,0 +1,15 @@
+<template>
+    <img :src="avatar" :alt="descricao">
+</template>
+
+<script>
+export default {
+    name: 'foto',
+    data () {
+        return {
+            avatar: "/img/imagem.jpeg",
+            descricao: "imagem teste"
+        }
+    }
+} 
+</script>
